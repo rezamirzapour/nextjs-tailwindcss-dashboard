b7734331fc5752cb99726883847e1577454de26d
Reza Mirzapour <mirzaandrampage@gmail.com>
2024-05-03 20:03:22 +0330

    feat(sign in): localization and improve components

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,11 +4,13 @@ import DropdownMessage from "./DropdownMessage";
 import DropdownNotification from "./DropdownNotification";
 import DropdownUser from "./DropdownUser";
 import Image from "next/image";
+import { useMessages } from "next-intl";
 
 const Header = (props: {
   sidebarOpen: string | boolean | undefined;
   setSidebarOpen: (arg0: boolean) => void;
 }) => {
+  const messages = useMessages();
   return (
     <header className="sticky top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
       <div className="flex flex-grow items-center justify-between px-4 py-4 shadow-2 md:px-6 2xl:px-11">
@@ -95,7 +97,7 @@ const Header = (props: {
 
               <input
                 type="text"
-                placeholder="Type to search..."
+                placeholder={messages.common.type_to_search}
                 className="w-full bg-transparent pl-9 pr-4 font-medium focus:outline-none xl:w-125"
               />
             </div>
