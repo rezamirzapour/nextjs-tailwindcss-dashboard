af0d88e60fe05c685c5a75bc23afd18fc469e8db
Reza Mirzapour <mirzaandrampage@gmail.com>
2024-05-04 11:33:47 +0330

    change(signin form): use input and button components

--- src/app/[locale]/(auth)/signin/components/Form.tsx
+++ src/app/[locale]/(auth)/signin/components/Form.tsx
@@ -7,6 +7,8 @@ import AuthService from "@/services/auth/AuthService";
 import clsx from "clsx";
 import { useMutation } from "@tanstack/react-query";
 import type { RegisterDto, VerifyDto } from "@/services/auth/AuthService.type";
+import Input from "@/components/Input";
+import Button from "@/components/Button";
 
 const authService = new AuthService();
 
@@ -25,7 +27,7 @@ const Form: React.FC = () => {
     mutateAsync: registerFn,
     isPending: isRegisterPending,
   } = useMutation({
-    mutationFn: (body) => authService.register(body),
+    mutationFn: (body) => authService.signup(body),
   });
 
   const {
@@ -56,77 +58,34 @@ const Form: React.FC = () => {
         </h2>
 
         <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="mb-4">
-            <label className="mb-2.5 block font-medium text-black dark:text-white">
-              {isWaitingForCode
-                ? messages.signIn.code
-                : messages.signIn.phone_number}
-            </label>
-            <div className={clsx("relative", { hidden: isWaitingForCode })}>
-              <input
-                {...register("phoneNumber")}
-                placeholder={messages.signIn.phone_number_placeholder}
-                className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-              />
-
-              <span className="absolute right-4 top-4">
-                <svg
-                  className="fill-current"
-                  width="22"
-                  height="22"
-                  viewBox="0 0 22 22"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <g opacity="0.5">
-                    <path
-                      d="M19.2516 3.30005H2.75156C1.58281 3.30005 0.585938 4.26255 0.585938 5.46567V16.6032C0.585938 17.7719 1.54844 18.7688 2.75156 18.7688H19.2516C20.4203 18.7688 21.4172 17.8063 21.4172 16.6032V5.4313C21.4172 4.26255 20.4203 3.30005 19.2516 3.30005ZM19.2516 4.84692C19.2859 4.84692 19.3203 4.84692 19.3547 4.84692L11.0016 10.2094L2.64844 4.84692C2.68281 4.84692 2.71719 4.84692 2.75156 4.84692H19.2516ZM19.2516 17.1532H2.75156C2.40781 17.1532 2.13281 16.8782 2.13281 16.5344V6.35942L10.1766 11.5157C10.4172 11.6875 10.6922 11.7563 10.9672 11.7563C11.2422 11.7563 11.5172 11.6875 11.7578 11.5157L19.8016 6.35942V16.5688C19.8703 16.9125 19.5953 17.1532 19.2516 17.1532Z"
-                      fill=""
-                    />
-                  </g>
-                </svg>
-              </span>
-            </div>
-            <div className={clsx("relative", { hidden: !isWaitingForCode })}>
-              <input
-                {...register("code")}
-                placeholder={messages.signIn.code_placeholder}
-                className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-              />
-
-              <span className="absolute right-4 top-4">
-                <svg
-                  className="fill-current"
-                  width="22"
-                  height="22"
-                  viewBox="0 0 22 22"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <g opacity="0.5">
-                    <path
-                      d="M19.2516 3.30005H2.75156C1.58281 3.30005 0.585938 4.26255 0.585938 5.46567V16.6032C0.585938 17.7719 1.54844 18.7688 2.75156 18.7688H19.2516C20.4203 18.7688 21.4172 17.8063 21.4172 16.6032V5.4313C21.4172 4.26255 20.4203 3.30005 19.2516 3.30005ZM19.2516 4.84692C19.2859 4.84692 19.3203 4.84692 19.3547 4.84692L11.0016 10.2094L2.64844 4.84692C2.68281 4.84692 2.71719 4.84692 2.75156 4.84692H19.2516ZM19.2516 17.1532H2.75156C2.40781 17.1532 2.13281 16.8782 2.13281 16.5344V6.35942L10.1766 11.5157C10.4172 11.6875 10.6922 11.7563 10.9672 11.7563C11.2422 11.7563 11.5172 11.6875 11.7578 11.5157L19.8016 6.35942V16.5688C19.8703 16.9125 19.5953 17.1532 19.2516 17.1532Z"
-                      fill=""
-                    />
-                  </g>
-                </svg>
-              </span>
-            </div>
-          </div>
-
+          <Input
+            labelTextSize="large"
+            containerProps={{
+              className: clsx("mb-4", { hidden: isWaitingForCode }),
+            }}
+            label={messages.signIn.phone_number}
+            placeholder={messages.signIn.phone_number_placeholder}
+            {...register("phoneNumber")}
+          />
+          <Input
+            labelTextSize="large"
+            containerProps={{
+              className: clsx("mb-4", { hidden: !isWaitingForCode }),
+            }}
+            label={messages.signIn.code}
+            placeholder={messages.signIn.code_placeholder}
+            {...register("code")}
+          />
           <div className="mb-5">
-            <input
+            <Button
               type="submit"
               disabled={isRegisterPending || isVerifyPending}
-              value={
-                isWaitingForCode
-                  ? messages.signIn.sign_in
-                  : messages.signIn.send_code
-              }
-              className={
-                "w-full cursor-pointer rounded-lg border border-primary bg-primary p-4 text-white transition hover:bg-opacity-90 disabled:bg-slate-500"
-              }
-            />
+              fullWidth
+            >
+              {isWaitingForCode
+                ? messages.signIn.sign_in
+                : messages.signIn.send_code}
+            </Button>
           </div>
 
           <button className="flex w-full items-center justify-center gap-3.5 rounded-lg border border-stroke bg-gray p-4 hover:bg-opacity-50 dark:border-strokedark dark:bg-meta-4 dark:hover:bg-opacity-50">
